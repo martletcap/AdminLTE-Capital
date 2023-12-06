1e846dd4480d9d48c8273348888df972352a0e8c
artur0527rg <rogozinskiyartur@gmail.com>
2023-12-06 13:57:58 +0300

    DateTime to Date; Sidebar

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from . import models
+
 # Register your models here.
+admin.site.register(models.Share)
+admin.site.register(models.Location)
+admin.site.register(models.Sector)
+admin.site.register(models.Company)
+admin.site.register(models.OurTransaction)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,73 @@
 from django.db import models
 
+
 # Create your models here.
+class Sector(models.Model):
+
+  class Meta:
+        db_table = 'sector'
+        ordering = ['name']
+
+  id = models.AutoField(primary_key=True)
+  name = models.CharField(max_length=128)
+
+  def __str__(self) -> str:
+      return self.name
+
+
+class Location(models.Model):
+
+  class Meta:
+        db_table = 'location'
+        ordering = ['city']
+
+  id = models.AutoField(primary_key=True)
+  city = models.CharField(max_length=128)
+
+  def __str__(self) -> str:
+      return self.city
+
+
+class Share(models.Model):
+
+  class Meta:
+        db_table = 'share'
+        ordering = ['name']
+
+  id = models.AutoField(primary_key=True)
+  name = models.CharField(max_length=256)
+
+  def __str__(self) -> str:
+      return self.name
+
+class Company(models.Model):
+
+  class Meta:
+        db_table = 'company'
+        ordering = ['name']
+
+  id = models.AutoField(primary_key=True)
+  name = models.CharField(max_length=256)
+  desctiption = models.TextField()
+  location = models.ForeignKey(Location, on_delete=models.PROTECT)
+  sector = models.ForeignKey(Sector, on_delete=models.PROTECT)
+
+  def __str__(self) -> str:
+      return self.name
+
+class OurTransaction(models.Model):
+
+  class Meta:
+        db_table = 'our_transaction'
+        ordering = ['-date']
+
+  id = models.AutoField(primary_key=True)
+  company = models.ForeignKey(Company, on_delete=models.PROTECT)
+  date = models.DateField()
+  amount = models.IntegerField()
+  price = models.DecimalField(max_digits=15, decimal_places=3)
+  total = models.IntegerField()
+  share = models.ForeignKey(Share, on_delete=models.PROTECT)
+
+  def __str__(self) -> str:
+      return str(self.date)
