59e23b0ea9e2e0a8d4f4b1ee420c7cb2c8272d38
artur0527rg <rogozinskiyartur@gmail.com>
2023-12-07 16:19:05 +0300

    Table fix

--- home/admin.py
+++ home/admin.py
@@ -1,16 +1,19 @@
 from django.contrib import admin
 
 from .models import Share, Location, Sector, Company, OurTransaction
-from .forms import OurTransactionForm
+from .forms import OurTransactionForm, CompanyForm
 
 class OurTransactionAdmin(admin.ModelAdmin):
+  list_display = ["company", "date", "amount", "price", "total", "share"]
   form = OurTransactionForm
 
-
+class CompanyAdmin(admin.ModelAdmin):
+  list_display = ["name", "location", "sector"]
+  form = CompanyForm
 
 # Register your models here.
 admin.site.register(Share)
 admin.site.register(Location)
 admin.site.register(Sector)
-admin.site.register(Company)
+admin.site.register(Company, CompanyAdmin)
 admin.site.register(OurTransaction, OurTransactionAdmin)
--- home/forms.py
+++ home/forms.py
@@ -1,9 +1,14 @@
 from django import forms
 
-from .models import OurTransaction
+from .models import OurTransaction, Company
 
 class OurTransactionForm(forms.ModelForm):
     class Meta:
         model = OurTransaction
         widgets = {'date':forms.widgets.NumberInput(attrs={'type':'date'})}
+        fields = "__all__"
+
+class CompanyForm(forms.ModelForm):
+    class Meta:
+        model = Company
         fields = "__all__"
