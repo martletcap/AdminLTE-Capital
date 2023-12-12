8b90d7b72da6c7e5d07b089b16e56ed470685335
artur0527rg <rogozinskiyartur@gmail.com>
2023-12-12 13:31:32 +0300

    DB required fields fix

--- home/models.py
+++ home/models.py
@@ -10,7 +10,7 @@ class ContactType(models.Model):
         db_table = 'contact_type'
 
     id = models.AutoField(primary_key=True)
-    type = models.CharField(max_length=128)
+    type = models.CharField(max_length=128, unique=True)
 
     def __str__(self)->str:
         return self.type
@@ -21,7 +21,7 @@ class Contact(models.Model):
     class Meta:
         db_table = 'contact'
 
-    name = models.CharField(max_length=256)
+    name = models.CharField(max_length=256, unique=True)
     email = models.EmailField(blank=True)
     phone = models.CharField(max_length=128, blank=True)
     comment = models.TextField(max_length=1024, blank=True)
@@ -39,7 +39,7 @@ class Sector(models.Model):
         ordering = ['name']
 
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=256)
+    name = models.CharField(max_length=256, unique=True)
 
     def __str__(self) -> str:
         return self.name
@@ -49,6 +49,7 @@ class Location(models.Model):
 
     class Meta:
         db_table = 'location'
+        unique_together = ('city', 'country',)
 
     id = models.AutoField(primary_key=True)
     city = models.CharField(max_length=128)
@@ -66,7 +67,7 @@ class CompanyStatus(models.Model):
         ordering = ['status']
 
     id = models.AutoField(primary_key=True)
-    status = models.CharField(max_length=256)
+    status = models.CharField(max_length=256, unique=True)
 
     def __str__(self) -> str:
         return self.status
@@ -79,7 +80,7 @@ class CategoryOfCompany(models.Model):
         ordering = ['category']
 
     id = models.AutoField(primary_key=True)
-    category = models.CharField(max_length=256)
+    category = models.CharField(max_length=256, unique=True)
 
     def __str__(self) -> str:
         return self.category
@@ -92,11 +93,16 @@ class Company(models.Model):
         ordering = ['name']
 
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=256)
+    name = models.CharField(max_length=256, unique=True)
     short_name = models.CharField(max_length=256, blank=True)
     comment = models.TextField(max_length=1024, blank=True)
     staff = models.ForeignKey(User, on_delete=models.PROTECT)
-    contact = models.ForeignKey(Contact, on_delete=models.PROTECT)
+    contact = models.ForeignKey(
+        Contact,
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+    )
     location = models.ForeignKey(Location, on_delete=models.PROTECT)
     sector = models.ForeignKey(Sector, on_delete=models.PROTECT)
     status = models.ForeignKey(CompanyStatus, on_delete=models.PROTECT)
@@ -126,7 +132,7 @@ class ShareType(models.Model):
         ordering = ['type']
 
     id = models.AutoField(primary_key=True)
-    type = models.CharField(max_length=256)
+    type = models.CharField(max_length=256, unique=True)
 
     def __str__(self)->str:
         return self.type
@@ -136,6 +142,7 @@ class Share(models.Model):
 
     class Meta:
         db_table = 'share'
+        unique_together = ('company', 'type')
 
     id = models.AutoField(primary_key=True)
     type = models.ForeignKey(ShareType, on_delete=models.PROTECT)
@@ -180,8 +187,12 @@ class OurTransaction(models.Model):
     id = models.AutoField(primary_key=True)
     share = models.ForeignKey(Share, on_delete=models.PROTECT)
     date = models.DateField()
-    amount = models.IntegerField()
-    price = models.DecimalField(max_digits=12, decimal_places=4)
+    amount = models.IntegerField(verbose_name='Amount of Shares')
+    price = models.DecimalField(
+        max_digits=16,
+        decimal_places=8,
+        verbose_name='Price per 1 share',
+    )
     add_by = models.ForeignKey(
         User, on_delete=models.PROTECT,
         related_name='our_transaction_add',
