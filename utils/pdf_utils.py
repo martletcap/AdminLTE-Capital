65b4631ed41eca354b2117ac832359d7974f8014
artur0527rg <rogozinskiyartur@gmail.com>
2024-01-19 12:38:07 +0300

    date fix

--- utils/pdf_utils.py
+++ utils/pdf_utils.py
@@ -51,7 +51,13 @@ def SH01_parser(pdf):
         text +=  page.extract_text()
 
     company_name =  re.search(r'Company Name:\s(.+)(?:\s+|$)', text).group(1)
-    date = re.search(r'\d{2}/\d{2}/\d{4}\s(\d{2}/\d{2}/\d{4})', text).group(1)
+    try:
+        date = re.search(r'\d{2}/\d{2}/\d{4}\s(\d{2}/\d{2}/\d{4})', text).group(1)
+    except AttributeError:
+        date = re.search(
+            r'Received for filing in Electronic Format on the: (\d{2}/\d{2}/\d{4})',
+            text,
+        ).group(1)
     date = datetime.strptime(date, '%d/%m/%Y').date()
 
     # Remove problem areas
