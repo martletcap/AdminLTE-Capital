2323549e303e480646d69a5d702a2073a2c66de0
artur0527rg <rogozinskiyartur@gmail.com>
2024-02-16 15:03:24 +0300

    share_name_correction new (NON-VOTING) pattern

--- utils/general.py
+++ utils/general.py
@@ -3,6 +3,7 @@ import re
 
 def share_name_correction(line:str):
     line = line.replace('SHARES', '')
+    line = line.replace('(NON-VOTING)', '')
     # Del smth like "0.0001 GBP..."
     flt = line.find('0.')
     if flt != -1:
